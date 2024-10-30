604af3bb4d037c49a849f2be02216a8bef7f378e
gaba-teixeira <gaba.andrade@outlook.com>
2024-10-29 21:26:29 -0300

    created js file using jQuery and edit css/html

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+    const novaLinha = $(`<li class='task-item'></li>`);
+    const novaTarefa = $("#nova-tarefa").val().toUpperCase();
+    $(novaLinha).text(novaTarefa);
+    $(novaLinha).appendTo(".task-list");
+    $("#nova-tarefa").val("");
+  });
+});
+$(".task-list").on("click", "li", function () {
+  $(this).toggleClass("task-feita");
+});
